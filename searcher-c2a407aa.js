7ee9ae8c7ab316732c1b5733fe2eb321b9da45cb
169LI <169LI@users.noreply.github.com>
2026-05-15 04:44:14 +0000

    deploy: f841872a1d267b8207cbc5ebe68283ebe80b79b3

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-df5c5b28.js',
+                path_to_root + 'searchindex-a0a07116.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
